1db1415d7e99ffb4886f5b6586e9b26407e01a12
AnanyaNagar <60837177+AnanyaNagar@users.noreply.github.com>
2021-11-17 23:54:17 +0530

    Adding python program implementing ML model, and serialized version of our model

--- app backend/app.py
+++ app backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("..\public\index.html")
 
 
 if(__name__ == "__main__"):
