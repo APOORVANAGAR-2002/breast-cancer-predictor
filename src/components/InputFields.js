a0d60d4c1f948ac65b166a68a1386515040d0483
APOORVANAGAR-2002 <apoorvanagar2002@gmail.com>
2021-11-18 00:47:17 +0530

    json data implementation of input fields

--- src/components/InputFields.js
+++ src/components/InputFields.js
@@ -1,4 +1,6 @@
 import { Button, Grid, makeStyles, TextField, Typography } from "@material-ui/core";
+import axios from 'axios';
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,9 +18,35 @@ const useStyles = makeStyles((theme) => ({
     }
 }))
 
+const initialData = {
+    radius: '',
+    texture: '',
+    perimeter: '',
+    area: '',
+    smoothness: ''
+}
+
 function InputFields() {
 
     const classes = useStyles();
+    const [data, setData] = useState(initialData);
+
+    let handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setData({
+            ...data,
+            [name]: value
+        })
+    }
+
+    let submitForm = (event) => {
+        console.log(data);
+        axios.post("http://localhost:5000/submit", data).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        }) 
+    }
 
     return (
         <Grid container style={{
@@ -27,23 +55,60 @@ function InputFields() {
             <Grid container component='form' className={classes.root}>
                 <Grid item className={classes.input}>
                     <Typography variant="body1">mean_radius</Typography>
-                    <TextField id="outlined-basic" label="Input-1" variant="outlined" size="small" />
+                    <TextField
+                        id="outlined-basic"
+                        label="Input-1"
+                        variant="outlined"
+                        size="small"
+                        name="radius"
+                    value={data.radius}
+                    onChange={handleInputChange}
+
+                    />
                 </Grid>
                 <Grid item className={classes.input}>
                     <Typography variant="body1">mean_texture</Typography>
-                    <TextField id="outlined-basic" label="Input-2" variant="outlined" size="small" />
+                    <TextField id="outlined-basic"
+                        label="Input-2"
+                        variant="outlined"
+                        size="small"
+                        name="texture"
+                        value={data.texture}
+                        onChange={handleInputChange}
+                        />
                 </Grid>
                 <Grid item className={classes.input}>
                     <Typography variant="body1">mean_perimeter</Typography>
-                    <TextField id="outlined-basic" label="Input-3" variant="outlined" size="small" />
+                    <TextField id="outlined-basic"
+                        label="Input-3"
+                        variant="outlined"
+                        size="small"
+                        name="perimeter"
+                        value={data.perimeter}
+                        onChange={handleInputChange}
+                        />
                 </Grid>
                 <Grid item className={classes.input}>
                     <Typography variant="body1">mean_area</Typography>
-                    <TextField id="outlined-basic" label="Input-4" variant="outlined" size="small" />
+                    <TextField id="outlined-basic"
+                        label="Input-4"
+                        variant="outlined"
+                        size="small"
+                        name="area"
+                        value={data.area}
+                        onChange={handleInputChange}
+                    />
                 </Grid>
                 <Grid item className={classes.input}>
                     <Typography variant="body1">mean_smoothness</Typography>
-                    <TextField id="outlined-basic" label="Input-5" variant="outlined" size="small" />
+                    <TextField id="outlined-basic"
+                        label="Input-5"
+                        variant="outlined"
+                        size="small"
+                        name="smoothness"
+                        value={data.smoothness}
+                        onChange={handleInputChange}
+                        />
                 </Grid>
             </Grid>
 
@@ -54,7 +119,7 @@ function InputFields() {
                 padding: '30px'
             }}>
                 <Grid item>
-                    <Button variant="contained" color="primary">Submit</Button>
+                    <Button variant="contained" color="primary" onClick={submitForm}>Submit</Button>
                 </Grid>
             </Grid>
 
